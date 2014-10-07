edb3b75dca2e796ac05fe6231fc2c221266e0857
slkennedy <slkennedy@gmail.com>
2014-10-07 18:43:00 -0400

    Starts Ex. D, adds router and views

--- app/scripts/d.js
+++ app/scripts/d.js
@@ -7,6 +7,18 @@
 	App.Views = {};
 	App.Collections = {};
 
+////////////////Routers///////
+/////////////////////////////
+
+	App.Router = Backbone.Router.extend ({
+
+		initialize: function (){
+			new App.Views.CreateBookmarkForm(),
+			new App.Views.BookmarksView(),
+			new App.Views.TagsView()
+		}
+	});
+
 ////////////////Models////////	
 /////////////////////////////
 
@@ -16,16 +28,53 @@
 ////////////////Views////////	
 ////////////////////////////
 
-	App.Views.CreateBookmarkFrom = Backbone.View.extend ({
+	App.Views.CreateBookmarkForm = Backbone.View.extend ({
 		tagName: 'form',
 		className: 'bookmark-form',
-		template: _.template
+		template: _.template($('#templates-bookmark-form').html()),
 
+		initialize: function (){
+			$('.inputs').append(this.el);
+			this.render();
+		},
+
+		render: function(){
+			this.$el.html(this.template);
+		}
+
+	});
+
+	App.Views.BookmarksView = Backbone.View.extend ({
+		tagName: 'ul',
+		className: 'bookmarks-view',
+
+		initialize: function (){
+			$('.lists').append(this.el);
+			this.render();
+		},
+
+		render: function (){
+		}
+	});
+
+	App.Views.TagsView = Backbone.View.extend ({
+		tagName: 'ul',
+		className: 'tags-view',
+
+		initialize: function (){
+			$('.lists').append(this.el);
+			this.render();
+		},
+
+		render: function (){
+
+		}
 	});
 
 ////////////////Glue Code/////	
 /////////////////////////////
 	$(document).ready(function (){
-
+		new App.Router();
+		Backbone.history.start();
 	});
 })();
